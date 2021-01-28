eee1f04848c579d8629e11d8a87ec67e10bfaa3d
Ehetesham Ali <ali.ehetesham909@gmail.com>
2021-01-28 16:49:42 +0530

    Ehetesham:-productInventoryChange

--- AccountErp.Models/Product/ProductInventoryJqDataTableRequestModel.cs
+++ AccountErp.Models/Product/ProductInventoryJqDataTableRequestModel.cs
@@ -9,9 +9,9 @@ namespace AccountErp.Models.Product
     {
         public string FilterKey { get; set; }
 
-        public DateTime StartDate {get; set; }
+        public DateTime? StartDate {get; set; }
 
-       public DateTime EndDate {get; set; }
+       public DateTime? EndDate {get; set; }
 
 
 }
