bd212cdfa54cbe79fd3323cdc039bbc4e1ee769d
shahanawazahemad30@gmail.com <shahanawazahemad30@gmail.com>
2021-01-29 12:17:02 +0530

    migration 29-01-2021

---
+++ AccountErp.DataLayer/Migrations/20210128070706_migration28012021.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AccountErp.DataLayer.Migrations
+{
+    public partial class migration28012021 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WareHouse",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(maxLength: 250, nullable: false),
+                    Location = table.Column<string>(maxLength: 1000, nullable: false),
+                    CreatedBy = table.Column<string>(maxLength: 40, nullable: false),
+                    UpdatedBy = table.Column<string>(maxLength: 40, nullable: true),
+                    CreatedOn = table.Column<DateTime>(nullable: false),
+                    UpdatedOn = table.Column<DateTime>(nullable: true),
+                    Status = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WareHouse", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WareHouse");
+        }
+    }
+}
